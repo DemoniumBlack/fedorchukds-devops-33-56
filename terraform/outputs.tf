f0a00763ee88c5dacd3bc84630e5b97c79f605e1
Demonium <demoniumxxx@gmail.com>
2024-06-09 22:33:43 +0700

    edit2

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -22,8 +22,3 @@ output "Web_App_Network_Load_Balancer_Address" {
   value = yandex_lb_network_load_balancer.nlb-web-app.listener.*.external_address_spec[0].*.address
   description = "Адрес сетевого балансировщика Web App"
 }
-
-output "Web_App_Network_Load_Balancer_Address" {
-  value = yandex_lb_network_load_balancer.nlb-web-app.listener.*.external_address_spec[0].*.address
-  description = "Адрес сетевого балансировщика Web App"
-}
